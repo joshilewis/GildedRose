be4fc7b2925034bba7de8897381351aa7d361597
Joshua Lewis <joshua.lewis@dstvdm.com>
2014-07-14 16:03:32 +0200

    Added Tests

---
+++ src/GildedRose.Tests/ProgramTests.cs
@@ -0,0 +1,69 @@
+using System.Collections.Generic;
+using GildedRose.Console;
+using NUnit.Framework;
+
+namespace GildedRose.Tests
+{
+    [TestFixture]
+    public class ProgramTests
+    {
+        [Test]
+        public void Test()
+        {
+            var expected = new List<Item>()
+            {
+                new Item {Name = "+5 Dexterity Vest", SellIn = 9, Quality = 19},
+                new Item {Name = "Aged Brie", SellIn = 1, Quality = 1},
+                new Item {Name = "Elixir of the Mongoose", SellIn = 4, Quality = 6},
+                new Item {Name = "Sulfuras, Hand of Ragnaros", SellIn = 0, Quality = 80},
+                new Item
+                {
+                    Name = "Backstage passes to a TAFKAL80ETC concert",
+                    SellIn = 14,
+                    Quality = 21
+                },
+                new Item {Name = "Conjured Mana Cake", SellIn = 2, Quality = 5}
+            };
+
+            var program = new Program()
+            {
+                Items = new List<Item>
+                                          {
+                                              new Item {Name = "+5 Dexterity Vest", SellIn = 10, Quality = 20},
+                                              new Item {Name = "Aged Brie", SellIn = 2, Quality = 0},
+                                              new Item {Name = "Elixir of the Mongoose", SellIn = 5, Quality = 7},
+                                              new Item {Name = "Sulfuras, Hand of Ragnaros", SellIn = 0, Quality = 80},
+                                              new Item
+                                                  {
+                                                      Name = "Backstage passes to a TAFKAL80ETC concert",
+                                                      SellIn = 15,
+                                                      Quality = 20
+                                                  },
+                                              new Item {Name = "Conjured Mana Cake", SellIn = 3, Quality = 6}
+                                          }
+
+            };
+
+            program.UpdateQuality();
+
+            Assert.That(program.Items, Is.EquivalentTo(expected).Using(new EqualityComparer()));
+
+        }
+    }
+
+    public class EqualityComparer : IEqualityComparer<Item>
+    {
+        public bool Equals(Item x, Item y)
+        {
+            return x.Name.Equals(y.Name) &&
+                   x.Quality.Equals(y.Quality) &&
+                   x.SellIn.Equals(y.SellIn)
+                ;
+        }
+
+        public int GetHashCode(Item obj)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
