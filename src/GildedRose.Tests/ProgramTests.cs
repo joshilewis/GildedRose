7e22f0c63509ab660a3d7cbe04944aeb401bdf89
Joshua Lewis <joshua.lewis@dstvdm.com>
2014-09-12 09:41:27 +0200

    Added individual tests for all cases mentioned in textual requirements description

--- src/GildedRose.Tests/ProgramTests.cs
+++ src/GildedRose.Tests/ProgramTests.cs
@@ -50,6 +50,177 @@ namespace GildedRose.Tests
 
         }
 
+        [Test]
+        public void ItemSellInAndQualityShouldDecreaseBy1()
+        {
+            var expected = new List<Item>()
+            {
+                new Item {Name = "+5 Dexterity Vest", SellIn = 9, Quality = 4},
+            };
+            var initial = new List<Item>
+            {
+                new Item {Name = "+5 Dexterity Vest", SellIn = 10, Quality = 5},
+            };
+
+            ActAndAssert(initial, expected);
+        }
+
+        [Test]
+        public void ItemQualityShouldNeverBeNegative()
+        {
+            var expected = new List<Item>()
+            {
+                new Item {Name = "+5 Dexterity Vest", SellIn = 4, Quality = 0},
+            };
+
+            var initial = new List<Item>
+            {
+                new Item {Name = "+5 Dexterity Vest", SellIn = 5, Quality = 0},
+            };
+
+            ActAndAssert(initial, expected);
+
+        }
+
+        [Test]
+        public void ItemQualityShouldDegradeTwiceAsFastAfterSellBy()
+        {
+            var expected = new List<Item>()
+            {
+                new Item {Name = "+5 Dexterity Vest", SellIn = -1, Quality = 8},
+            };
+
+            var initial = new List<Item>
+            {
+                new Item {Name = "+5 Dexterity Vest", SellIn = 0, Quality = 10},
+            };
+
+            ActAndAssert(initial, expected);
+        }
+
+        [Test]
+        public void AgedBrieShouldIncreaseInQualityTheOlderItGets()
+        {
+            var expected = new List<Item>()
+            {
+                new Item {Name = "Aged Brie", SellIn = 4, Quality = 6},
+            };
+
+            var initial = new List<Item>
+            {
+                new Item {Name = "Aged Brie", SellIn = 5, Quality = 5},
+            };
+
+            ActAndAssert(initial, expected);
+        }
+
+        [Test]
+        public void AgedBrieQualityShouldNeverIncreaseAbove50()
+        {
+            var expected = new List<Item>()
+            {
+                new Item {Name = "Aged Brie", SellIn = 4, Quality = 50},
+            };
+
+            var initial = new List<Item>
+            {
+                new Item {Name = "Aged Brie", SellIn = 5, Quality = 50},
+            };
+
+            ActAndAssert(initial, expected);
+        }
+
+        [Test]
+        public void SulfurasItemsShouldNeverChange()
+        {
+            var expected = new List<Item>()
+            {
+                new Item {Name = "Sulfuras, Hand of Ragnaros", SellIn = 0, Quality = 80},
+            };
+
+            var initial = new List<Item>
+            {
+                new Item {Name = "Sulfuras, Hand of Ragnaros", SellIn = 0, Quality = 80},
+            };
+
+            ActAndAssert(initial, expected);
+        }
+
+        [Test]
+        public void BackstagePassesQualityShouldIncreaseNormallyBefore10DaysToGo()
+        {
+            var expected = new List<Item>()
+            {
+                new Item { Name = "Backstage passes to a TAFKAL80ETC concert", SellIn = 14, Quality = 21 },
+            };
+
+            var initial = new List<Item>
+            {
+                new Item { Name = "Backstage passes to a TAFKAL80ETC concert", SellIn = 15, Quality = 20 },
+            };
+
+            ActAndAssert(initial, expected);
+        }
+
+        [Test]
+        public void BackstagePassesQualityShouldDoubleBetween10And5DaysToGo()
+        {
+            var expected = new List<Item>()
+            {
+                new Item { Name = "Backstage passes to a TAFKAL80ETC concert", SellIn = 9, Quality = 22 },
+            };
+
+            var initial = new List<Item>
+            {
+                new Item { Name = "Backstage passes to a TAFKAL80ETC concert", SellIn = 10, Quality = 20 },
+            };
+
+            ActAndAssert(initial, expected);
+        }
+
+        [Test]
+        public void BackstagePassesQualityShouldIncreaseBy3Between3And0DaysToGo()
+        {
+            var expected = new List<Item>()
+            {
+                new Item { Name = "Backstage passes to a TAFKAL80ETC concert", SellIn = 3, Quality = 23 },
+            };
+
+            var initial = new List<Item>
+            {
+                new Item { Name = "Backstage passes to a TAFKAL80ETC concert", SellIn = 4, Quality = 20 },
+            };
+
+            ActAndAssert(initial, expected);
+        }
+
+        [Test]
+        public void BackstagePassesQualityShouldBe0AfterConcert()
+        {
+            var expected = new List<Item>()
+            {
+                new Item { Name = "Backstage passes to a TAFKAL80ETC concert", SellIn = -1, Quality = 0 },
+            };
+
+            var initial = new List<Item>
+            {
+                new Item { Name = "Backstage passes to a TAFKAL80ETC concert", SellIn = 0, Quality = 20 },
+            };
+
+            ActAndAssert(initial, expected);
+        }
+
+        private void ActAndAssert(List<Item> initial, List<Item> expected)
+        {
+            var program = new Program()
+            {
+                Items = initial,
+            };
+
+            program.UpdateQuality();
+
+            Assert.That(program.Items, Is.EquivalentTo(expected).Using(new EqualityComparer()));
+        }
     }
 
     public class EqualityComparer : IEqualityComparer<Item>
